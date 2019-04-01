856df7f4942a5568b15c46ef4240b5b4b38d7da5
aperezcesar <aperezcesar@nautilus1.danielcastelao.org>
2019-04-01 11:35:17 +0200

    commit final

--- src/boletin24/Boletin24.java
+++ src/boletin24/Boletin24.java
@@ -1,12 +1,15 @@
-
 package boletin24;
 
 public class Boletin24 {
 
     public static void main(String[] args) {
-        Ventana v = new Ventana();
-        v.crearVentana();
-        
+        //ejecutamos la ventana
+//        Ventana v = new Ventana();
+//        v.crearVentana();
+
+        //ejecutamos la paleta
+        Paleta p = new Paleta();
+        p.setVisible(true);
     }
-    
+
 }
